aa1a41854352d95e397f69fbf496545ff9f78700
Theo LEPAGE <theo.lepage@outlook.com>
2019-04-05 22:38:17 -0700

    Implement PC and COND registers. Implement AND and NOT.

--- main.c
+++ main.c
@@ -2,24 +2,40 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-enum operations {
+enum {
     OP_BR, OP_ADD, OP_LD, OP_ST, OP_JSR, OP_AND, OP_LDR, OP_STR,
     OP_RTI, OP_NOT, OP_STI, OP_JMP, OP_RES, OP_LEA, OP_TRAP
 };
 
-enum registers {
-    R_R0, R_R1, R_R2, R_R3, R_R4, R_R5, R_R6, R_R7
+enum {
+    R_R0, R_R1, R_R2, R_R3, R_R4, R_R5, R_R6, R_R7, R_PC, R_COND, R_COUNT
 };
 
 uint16_t memory[UINT16_MAX];
-uint16_t registers[8];
+uint16_t registers[R_COUNT]; // R_COUNT = 11
 
 uint16_t sign_extend(uint16_t x, int n)
 {
     if ((x >> (n - 1)) & 1) {
-        x |= (0xffff << n); // signed
+        x |= (0xFFFF << n); // signed
     }
     return x; // unsigned
+}
+
+void update_cond(uint16_t r)
+{
+    if (registers[r] == 0)
+    {
+        registers[R_COND] = 1;
+    }
+    else if (registers[r] >> 15)
+    {
+        registers[R_COND] = 2;
+    }
+    else
+    {
+        registers[R_COND] = 4;
+    }
 }
 
 // Functions to parse an instruction 
@@ -41,32 +57,22 @@ uint16_t binToDec(char* bin) {
 
 int main()
 {
-    /*
-    // Open input file
-    FILE *fp = fopen("./input", "r");
-    if (fp == NULL)
-    {
-        perror("Error while opening the file.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Read input file
-    char ch;
-    int i = 0;
-    while((ch = fgetc(fp)) != EOF)
-    {
-        instruction[i] = ch;
-        i++;
-    }*/
+    // int running = 1;
+    // registers[R_PC] = 0x3000;
+    // while (running)
+    // {
+    //     uint16_t instruction = memory[registers[R_PC]++];
+    // }
     
     // Handle instruction
     uint16_t instruction = binToDec("0001010001000000"); //5184
     uint16_t operation = getOperationCode(instruction);
+    uint16_t sr1, dr;
     switch(operation) {
-        case OP_ADD:;
-            uint16_t sr1 = getFirstOperand(instruction);
-            uint16_t dr = getDestination(instruction);
-            if ((instruction >> 5) & 0x1) // Handle immediate values
+        case OP_ADD:
+            sr1 = getFirstOperand(instruction);
+            dr = getDestination(instruction);
+            if ((instruction >> 5) & 0x1)
             {
                 registers[dr] = registers[sr1] + sign_extend(instruction & 0x1F, 5);
             }
@@ -74,6 +80,39 @@ int main()
             {
                 registers[dr] = registers[sr1] + registers[getSecondOperand(instruction)];
             }
+            update_cond(dr);
+            break;
+
+        case OP_AND:
+            sr1 = getFirstOperand(instruction);
+            dr = getDestination(instruction);
+            if ((instruction >> 5) & 0x1)
+            {
+                registers[dr] = registers[sr1] & sign_extend(instruction & 0x1F, 5);
+            }
+            else
+            {
+                registers[dr] = registers[sr1] & registers[getSecondOperand(instruction)];
+            }
+            update_cond(dr);
+            break;
+
+        case OP_NOT:
+            sr1 = getFirstOperand(instruction);
+            dr = getDestination(instruction);
+            registers[dr] = ~registers[sr1];
+            update_cond(dr);
+            break;
+
+        case OP_BR:
+            // uint16_t offset = instruction & 0x1FF;
+            // uint16_t n = (instruction >> 11) & 0x1;
+            // uint16_t z = (instruction >> 10) & 0x1;
+            // uint16_t p = (instruction >> 9) & 0x1;
+            // if(registers[R_COND])
+            // {
+            //     registers[R_PC] += sign_extend(offset, 9);
+            // }
             break;
 
         default:
@@ -81,9 +120,6 @@ int main()
             abort();
             break;
     }
-
-    // Close input file
-    // fclose(fp);
 
     return 0;
 }
