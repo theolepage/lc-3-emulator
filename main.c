3eb2a0130aa7e2c480d856647606a327e9f5b798
Theo LEPAGE <theo.lepage@outlook.com>
2019-04-10 12:45:21 -0700

    Implement JSR, LDR and STR.

--- main.c
+++ main.c
@@ -67,62 +67,102 @@ int main()
     // Handle instruction
     uint16_t instruction = binToDec("0001010001000000"); //5184
     uint16_t operation = getOperationCode(instruction);
-    uint16_t sr1, dr;
+    uint16_t sr1, sr2, dr, offset6, offset9, offset11, nzp, baser;
     switch(operation) {
-        case OP_BR:;
-            uint16_t offset = instruction & 0x1FF;
-            uint16_t nzp = (instruction >> 9) & 0x7;
+        case OP_BR:
+            offset9 = instruction & 0x1FF;
+            nzp = (instruction >> 9) & 0x7;
+
             if(registers[R_COND] & nzp)
             {
-                registers[R_PC] += sign_extend(offset, 9);
+                registers[R_PC] += sign_extend(offset9, 9);
             }
             break;
 
         case OP_ADD:
             sr1 = getFirstOperand(instruction);
+            sr2 = getSecondOperand(instruction);
             dr = getDestination(instruction);
+
             if ((instruction >> 5) & 0x1)
             {
                 registers[dr] = registers[sr1] + sign_extend(instruction & 0x1F, 5);
             }
             else
             {
-                registers[dr] = registers[sr1] + registers[getSecondOperand(instruction)];
+                registers[dr] = registers[sr1] + registers[sr2];
             }
             update_cond(dr);
             break;
 
-        case OP_LD:;
-            uint16_t offset = instruction & 0x1FF;
+        case OP_LD:
+            offset9 = instruction & 0x1FF;
             dr = getDestination(instruction);
-            memory[dr] = memory[registers[R_PC] + offset];
+
+            memory[dr] = memory[registers[R_PC] + sign_extend(offset9, 9)];
             update_cond(dr);
             break;
 
-        case OP_ST:;
-            uint16_t offset = instruction & 0x1FF;
+        case OP_ST:
+            offset9 = instruction & 0x1FF;
             dr = getDestination(instruction);
-            memory[registers[R_PC] + offset] = memory[dr];
+
+            memory[registers[R_PC] + sign_extend(offset9, 9)] = memory[dr];
             update_cond(dr);
+            break;
+
+        case OP_JSR:
+            offset11 = instruction & 0x7FF;
+            baser = (instruction >> 6) & 0x7;
+
+            registers[R_R7] = registers[R_PC];
+            if ((instruction >> 11) & 0x1)
+            {
+                registers[R_PC] = baser;
+            }
+            else
+            {
+                registers[R_PC] += sign_extend(offset11, 11);
+            }
             break;
 
         case OP_AND:
             sr1 = getFirstOperand(instruction);
+            sr2 = getSecondOperand(instruction);
             dr = getDestination(instruction);
+
             if ((instruction >> 5) & 0x1)
             {
                 registers[dr] = registers[sr1] & sign_extend(instruction & 0x1F, 5);
             }
             else
             {
-                registers[dr] = registers[sr1] & registers[getSecondOperand(instruction)];
+                registers[dr] = registers[sr1] & registers[sr2];
             }
             update_cond(dr);
+            break;
+
+        case OP_LDR:
+            dr = getDestination(instruction);
+            baser = (instruction >> 6) & 0x7;
+            offset6 = instruction & 0x3F;
+
+            memory[dr] = memory[baser + sign_extend(offset6, 6)];
+            update_cond(dr);
+            break;
+
+        case OP_STR:
+            sr1 = getDestination(instruction);
+            baser = (instruction >> 6) & 0x7;
+            offset6 = instruction & 0x3F;
+
+            memory[baser + sign_extend(offset6, 6)] = sr1;
             break;
 
         case OP_NOT:
             sr1 = getFirstOperand(instruction);
             dr = getDestination(instruction);
+
             registers[dr] = ~registers[sr1];
             update_cond(dr);
             break;
